93f43d911576dfacfdda8aacd0c7376ce1d0e0da
louisdante9 <louisdante9@gmail.com>
2018-04-05 13:28:54 +0100

    initial commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andeladeveloper
+ * Date: 12/03/2018
+ * Time: 12:26 PM
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+
+class LoginController extends Controller {
+    public function login(Request $request) {
+
+        $rules = [
+            'email' => 'required',
+            'password' => 'required'
+        ];
+
+        $customMessage = [
+            'required' => 'Please fill attribute :attribute'
+        ];
+
+        $this->validate($request, $rules, $customMessage);
+        $email = $request->input('email');
+        try {
+            $login = User::where('email', $email)->first();
+            if ($login && $login->count() > 0 && Hash::check($request->input('password'), $login->password)) {
+                try {
+                    $api_token = sha1($login->id_user.time());
+
+                    User::where('id', $login->id_user)->update(['api_token' => $api_token]);
+                    $res['status'] = true;
+                    $res['message'] = 'Success login';
+                    $res['data'] =  $login;
+                    $res['api_token'] =  $api_token;
+
+                    return response($res, 200);
+
+
+                } catch (QueryException $ex) {
+                    $res['status'] = false;
+                    $res['message'] = $ex->getMessage();
+                    return response($res, 500);
+                }
+            } else {
+                $res['success'] = false;
+                $res['message'] = 'Username / email / password  not found';
+                return response($res, 401);
+            }
+//            if ($login) {
+//                if ($login->count() > 0) {
+//                    if (Hash::check($request->input('password'), $login->password)) {
+//                        try {
+//                            $api_token = sha1($login->id_user.time());
+//
+//                            $create_token = User::where('id', $login->id_user)->update(['api_token' => $api_token]);
+//                            $res['status'] = true;
+//                            $res['message'] = 'Success login';
+//                            $res['data'] =  $login;
+//                            $res['api_token'] =  $api_token;
+//
+//                            return response($res, 200);
+//
+//
+//                        } catch (\Illuminate\Database\QueryException $ex) {
+//                            $res['status'] = false;
+//                            $res['message'] = $ex->getMessage();
+//                            return response($res, 500);
+//                        }
+//                    } else {
+//                        $res['success'] = false;
+//                        $res['message'] = 'Username / email / password not found';
+//                        return response($res, 401);
+//                    }
+//                } else {
+//                    $res['success'] = false;
+//                    $res['message'] = 'Username / email / password  not found';
+//                    return response($res, 401);
+//                }
+//            } else {
+//                $res['success'] = false;
+//                $res['message'] = 'Username / email / password not found';
+//                return response($res, 401);
+//            }
+        } catch (QueryException $ex) {
+            $res['success'] = false;
+            $res['message'] = $ex->getMessage();
+            return response($res, 500);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+$router->get('/hello/world', function() use ($router){
+    return "hello world";
+});
+$router->get('/hello/{name}', function($name) use ($router) {
+    return "hello {$name}";
+});
+$router->post('/login', 'LoginController@login');
+$router->get('/request', function (Illuminate\Http\Request $request){
+    return 'Hello '.$request->get('name', 'stranger');
+});
+$router->get('/response', function (Illuminate\Http\Request $request){
+    if($request->wantsJson()){
+        return response()->json(['greeting' => 'Hello stranger']);
+    }
+//    return(new Illuminate\Http\Response('Hello stranger', 200))
+//        ->header('Content-type', 'text/plain');
+    return response()
+        ->make('Hello stranger', 200, ['Content-type'=> 'text/plain']);
+});
+$router->post('/register', 'UserController@register');
+$router->get('/user', ['middleware' => 'auth', 'uses' =>  'UserController@get_user']);
